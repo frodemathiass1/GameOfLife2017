146301b3c1321b430cc6d1d938ea37a9e3908888
Frode Mathiassen <s305449@stud.hioa.no>
2017-02-13 21:39:03 +0100

    Trying out diffrent stuff... nothing works, but still runs :p

--- src/gol/Controller.java
+++ src/gol/Controller.java
@@ -1,8 +1,41 @@
 package gol;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
 
-    // test comment
+public class Controller implements Initializable {
+
+    //@FXML private Canvas canvas;
+    @FXML private Button startBtn;
+    //@FXML private Button resetBtn;
+    //@FXML private ColorPicker colorPicker;
+    //@FXML private Slider sizeSlider;
+
+
+    @Override
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+
+        //colorPicker.setValue(Color.BLUE);
+        // draw();
+
+    }
+
+    public void startBtn(ActionEvent event){
+
+        startBtn.setText("Pause");
+    }
+
+
+
+
+
 
 
 }
--- src/gol/Main.java
+++ src/gol/Main.java
@@ -1,29 +1,47 @@
 package gol;
 
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("Gui.fxml"));
         primaryStage.setTitle("Game of Life");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
 
+        gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);
 
     }
+
+    Canvas canvas = new Canvas(200,200);
+    GraphicsContext gc;
+
+
+
+
+
+
 
 
     public static void main(String[] args) {
 
         launch(args);
-
 
     }
 }
