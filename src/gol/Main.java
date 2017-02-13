bb1ab32b3b948c5ec6161fe6535f1efd06c7e356
Frode Mathiassen <s305449@stud.hioa.no>
2017-02-13 16:11:05 +0100

    cleanup

--- src/gol/Main.java
+++ src/gol/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,15 +16,14 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
 
-        //This is a test comment
 
-        //This is another test
-
-        //tset
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
+
+
     }
 }
