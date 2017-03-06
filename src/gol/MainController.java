f51ec577fe8c24c710ce4f9cbc90ff12b1e72c1f
Frode Mathiassen <s305449@stud.hioa.no>
2017-03-06 18:09:05 +0100

    add sizeSliderHandler() method

--- src/gol/MainController.java
+++ src/gol/MainController.java
@@ -16,9 +16,7 @@ import javafx.scene.paint.Color;
 public class MainController implements Initializable {
 
     public Board board;
-    public int cellSize = 20;
-    //private Cell[][] cells;
-
+    public int cellSize = 10;
 
     // Internal GUI objects
     @FXML private Button startBtn, resetBtn, exitApp;
@@ -26,32 +24,18 @@ public class MainController implements Initializable {
     @FXML private Canvas canvas;
     @FXML private Slider sizeSlider;
 
-    /*
-    * Initializes application, with 'new board constructor' which
-    * takes the graphic content from canvas, and cellsize as argument
-    */
 
-    // Initialize at application startup
+
     @Override
     public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
         GraphicsContext graphics = canvas.getGraphicsContext2D();
         this.board=new Board(graphics, this.cellSize); // this is dependency injection!
         colorPick.setValue(Color.BLACK);
         this.board.drawGrid();
+        sizeSliderHandler();
 
 
 
-        sizeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable,
-                                Number oldValue, Number newValue) {
-                setCellSize(newValue.intValue());
-                //System.out.println(board.getColor());
-                //System.out.println(newValue.intValue() );
-
-                // Trenger et updateCanvas metodeKall her? Skriver over gammel grid slik at gamle rutenett henger igjen
-            }
-        });
     }
 
     public void setCellSize(int cellSize) {
@@ -59,15 +43,7 @@ public class MainController implements Initializable {
         this.board.drawGrid();
     }
 
-    /*
-    * Responsible for handling mouseClicks on canvas.
-    * Finds the x/y coordinates sets these coordinates to all
-    * new Cells objects that are instantiated and loaded into the Multidimimensional array
-    * which is initialized in Step 3.
-    *
-    * All cells are instantiated at initialisation but will not become visible until user
-    *  sets the cell to Alive or Dead by toggling the same mouse position on canvas
-    * */
+
     @FXML
     public void getCellPosition(MouseEvent event){
 
@@ -96,10 +72,20 @@ public class MainController implements Initializable {
 
 
 
-
-
-
     // Button & Slider Event handling
+    @FXML
+    public void sizeSliderHandler(){
+        sizeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                                Number oldValue, Number newValue) {
+                setCellSize(newValue.intValue());
+                //System.out.println(board.getColor());
+                //System.out.println(newValue.intValue() );
+            }
+        });
+
+    }
     @FXML
     public void startPause(){
         board.nextGeneration();
@@ -111,7 +97,6 @@ public class MainController implements Initializable {
         graphics.clearRect(0,0,600,400);
         board.clearBoard(board.getGrid());
         board.drawGrid();
-
     }
 
     @FXML
