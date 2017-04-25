3ac802fc6547d9d8fc738013bbcf834d6bbdaacf
Frode Mathiassen <s305449@stud.hioa.no>
2017-04-25 15:21:04 +0200

    image package move

--- src/main/gol/controller/GUIController.java
+++ src/main/gol/controller/GUIController.java
@@ -118,7 +118,6 @@ public class GUIController implements Initializable {
             dialog.oops();
             quit();
         }
-
     }
 
     //================================================================================
